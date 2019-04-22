b8a7398b9cd8eedd5d5253d81c2617831cddb7c2
ChauncyWang <chauncy_wang@163.com>
2019-04-22 18:27:28 +0800

    update file at 2019-04-22 18:27

--- src/test/java/SpringTest5.java
+++ src/test/java/SpringTest5.java
@@ -6,7 +6,7 @@ import chauncy.service.UserService;
 public class SpringTest5 {
 	public static void main(String[] args) {
 		// 1.先加载spring容器
-		ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext("spring4.xml");
+		ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext("spring5.xml");
 		System.out.println("Spring容器被加载");
 		// 2.使用bean的id查找对象
 		UserService userService = (UserService) applicationContext.getBean("userService");
